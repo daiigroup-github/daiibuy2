59f7fd00a8c5252b86d1446ba36ac575f6dbdf2f
xaqu <chilloutto@gmail.com>
2014-09-15 11:00:17 +0700

    commute home my file
    commute home my file

--- protected/modules/myfile/controllers/DefaultController.php
+++ protected/modules/myfile/controllers/DefaultController.php
@@ -3,10 +3,66 @@
 class DefaultController extends MasterController
 {
 
-	public $layout = '//layouts/cl1';
+//	public $layout = '//layouts/cl1';
 
 	public function actionIndex()
 	{
+		$this->nav = array(
+			array(
+				'url'=>'#',
+				'color'=>'green',
+				'caption'=>'M-WALL',
+				'description'=>'Description'
+			),
+			array(
+				'url'=>'#',
+				'color'=>'blue',
+				'caption'=>'DOUBLE-S',
+				'description'=>'Description'
+			),
+			array(
+				'url'=>'#',
+				'color'=>'red',
+				'caption'=>'SANDY',
+				'description'=>'Description'
+			),
+			array(
+				'url'=>'#',
+				'color'=>'orange',
+				'caption'=>'BRICKS',
+				'description'=>'Description'
+			),
+			array(
+				'url'=>'#',
+				'caption'=>'ABOUT FENZER',
+				'description'=>'Company Profile'
+			),
+		);
+
+
+		$this->sideBarCategories = array(
+			'title'=>'MyFile Categories',
+			'items'=>array(
+				array(
+					'link'=>'Atechwindow',
+					'url'=>'#'
+				),
+				array(
+					'link'=>'Fenzer',
+					'url'=>'#'
+				),
+				array(
+					'link'=>'Ginza Home',
+					'url'=>'#'
+				),
+				array(
+					'link'=>'Madrid Bathroom',
+					'url'=>'#'
+				),
+			)
+		);
+
+
 		$suppliers = array(
 			'atechwindow'=>'Atech Window',
 			'fenzer'=>'Fenzer',
