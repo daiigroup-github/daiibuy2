5a8451021e3e25850ae717d2887bdce695a3c1c0
xaqu <chilloutto@gmail.com>
2014-09-15 09:14:45 +0700

    edit my file default page
    my files default page

--- protected/modules/myfile/controllers/DefaultController.php
+++ protected/modules/myfile/controllers/DefaultController.php
@@ -1,9 +1,20 @@
 <?php
 
-class DefaultController extends Controller
+class DefaultController extends MasterController
 {
+
+	public $layout = '//layouts/cl1';
+
 	public function actionIndex()
 	{
-		$this->render('index');
+		$suppliers = array(
+			'atechwindow'=>'Atech Window',
+			'fenzer'=>'Fenzer',
+			'ginzahome'=>'Ginza Home',
+			'madrid'=>'Madrid Bathroom',
+		);
+		$this->render('index', array(
+			'suppliers'=>$suppliers));
 	}
+
 }
--- protected/modules/myfile/views/default/index.php
+++ protected/modules/myfile/views/default/index.php
@@ -1,17 +1,53 @@
 <?php
 /* @var $this DefaultController */
 
-$this->breadcrumbs=array(
+$this->breadcrumbs = array(
 	$this->module->id,
 );
 ?>
-<h1><?php echo $this->uniqueId . '/' . $this->action->id; ?></h1>
+<div class="row">
+	<!-- Heading -->
+	<div class="col-lg-12 col-md-12 col-sm-12">
 
-<p>
-This is the view content for action "<?php echo $this->action->id; ?>".
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+		<div class="carousel-heading">
+			<h4>My Files</h4>
+		</div>
+
+	</div>
+	<!-- /Heading -->
+</div>
+<div class="row">
+
+	<?php $i = 0; ?>
+	<?php foreach($suppliers as $key=> $value): ?>
+		<?php
+		$class = 'col-lg-3 col-md-3 col-sm-12';
+		//$class = ($i==0) ? 'col-lg-12 col-md-12 col-sm-12' : 'col-lg-4 col-md-4 col-sm-12';
+		//$class = 'col-lg-6 col-md-6 col-sm-12';
+		?>
+		<div class="<?php echo $class; ?>">
+			<div class="blog-item">
+
+				<a href="<?php echo Yii::app()->createUrl($key); ?>"><?php echo CHtml::image(Yii::app()->baseUrl . '/images/supplier/' . $key . '.jpg'); ?>
+					<div class="button" style="text-align: center;background-clip: border-box;
+						 background-color: rgb(52, 152, 219);"><?php echo $key; ?></div>
+				</a>
+				<?php
+				/*
+				  <div class="product-actions blog-actions">
+				  <span class="product-action dark-blue current">
+				  <span class="action-wrapper">
+				  <i class="icons icon-doc-text"></i>
+				  <span class="action-name">Read more</span>
+				  </span>
+				  </span>
+				  </div>
+				 */
+				?>
+			</div>
+
+		</div>
+		<?php $i++; ?>
+	<?php endforeach; ?>
+
+</div>
