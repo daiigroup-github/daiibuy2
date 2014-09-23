034873873fc520d5b88b825c10eee808baf26596
kamyjapkung <kamyjap@gmail.com>
2014-09-23 14:59:12 +0700

    xx

---
+++ protected/modules/backoffice/views/productOptionGroup/view.php
@@ -0,0 +1,70 @@
+<?php
+/* @var $this ProductOptionGroupController */
+/* @var $model ProductOptionGroup */
+
+$this->breadcrumbs = array(
+	'Product Option Groups'=>array(
+		'index'),
+	$model->title,
+);
+
+$this->menu = array(
+	array(
+		'label'=>'List ProductOptionGroup',
+		'url'=>array(
+			'index')),
+	array(
+		'label'=>'Create ProductOptionGroup',
+		'url'=>array(
+			'create')),
+	array(
+		'label'=>'Update ProductOptionGroup',
+		'url'=>array(
+			'update',
+			'id'=>$model->productOptionGroupId)),
+	array(
+		'label'=>'Delete ProductOptionGroup',
+		'url'=>'#',
+		'linkOptions'=>array(
+			'submit'=>array(
+				'delete',
+				'id'=>$model->productOptionGroupId),
+			'confirm'=>'Are you sure you want to delete this item?')),
+	array(
+		'label'=>'Manage ProductOptionGroup',
+		'url'=>array(
+			'admin')),
+);
+?>
+
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		View ProductOptionGroup #<?php echo $model->productOptionGroupId; ?>		<div class="pull-right">
+	<?php echo CHtml::link('<i class="icon-plus-sign"></i> Create', $this->createUrl('create'), array(
+		'class'=>'btn btn-xs btn-primary')); ?>
+		</div>
+	</div>
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'htmlOptions'=>array(
+		'class'=>'table table-bordered table-striped table-hover'),
+	'attributes'=>array(
+		'productOptionGroupId',
+		'productId',
+		'title',
+		'description',
+		array(
+			'name'=>'image',
+			'type'=>'image',
+			'value'=>Yii::app()->baseUrl . $model->image,
+		),
+		'sortOrder',
+		'status',
+		'createDateTime',
+		'updateDateTime',
+	),
+));
+?>
+</div>
