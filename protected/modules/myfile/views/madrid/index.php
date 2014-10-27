08993e16ec1038a51e60eec25c560b55325dd44d
kamyjapkung <kamyjap@gmail.com>
2014-10-27 10:16:32 +0700

    Myfile Madrid Index

--- protected/modules/myfile/views/madrid/index.php
+++ protected/modules/myfile/views/madrid/index.php
@@ -12,8 +12,8 @@ $this->breadcrumbs = array(
 		<?php foreach($myfileArray as $myfile): ?>
 			<div class='col-lg-3 col-md-3 col-sm-12'>
 				<div class="blog-item">
-					<a class="btn btn-primary col-md-12"  href="<?php echo Yii::app()->createUrl('/index.php/myfile/madrid/view/id/' . $myfile->orderId); ?>">
-						<h3><?php echo $myfile->title; ?></h3>
+					<a class="btn <?php echo ($myfile->status == 1) ? "btn-success" : "btn-primary" ?> col-md-12"  href="<?php echo Yii::app()->createUrl('/index.php/myfile/madrid/view/id/' . $myfile->orderId); ?>">
+						<h3><?php echo $myfile->title; ?><?php if($myfile->status == 1): ?><i class="fa fa-comments pull-left"></i><?php endif; ?></h3>
 					</a>
 				</div>
 			</div>
