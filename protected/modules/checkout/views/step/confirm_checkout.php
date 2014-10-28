ebeab2550d81d928e7b32b19fc04037ce3421dc0
kamyjapkung <kamyjap@gmail.com>
2014-10-28 22:47:04 +0700

    e-Payment

--- protected/modules/checkout/views/step/confirm_checkout.php
+++ protected/modules/checkout/views/step/confirm_checkout.php
@@ -1,11 +1,11 @@
-<?php // include 'e_payment/security.php'                                                             ?>
+<?php // include 'e_payment/security.php'                                                                ?>
 <script>
 //	function checkMinimum(subTotal)
 //	{
 //		subTotal = subTotal.replace(",", "");
-//		var minimun = <?php // echo number_format($model->supplier->minimumOrder, 2, ".", "");                ?>;
-//		lblMinError = document.getElementById("lblMinimumError<?php // echo $model->supplier->supplierId;                ?>");
-//		btnCheckout = document.getElementById("btnCheckout<?php // echo $model->supplier->supplierId;                ?>");
+//		var minimun = <?php // echo number_format($model->supplier->minimumOrder, 2, ".", "");                   ?>;
+//		lblMinError = document.getElementById("lblMinimumError<?php // echo $model->supplier->supplierId;                   ?>");
+//		btnCheckout = document.getElementById("btnCheckout<?php // echo $model->supplier->supplierId;                   ?>");
 //		if (subTotal >= minimun)
 //		{
 //			lblMinError.style.display = "none";
@@ -31,27 +31,33 @@ $ePayment = Supplier::model()->findEpaymentByConfig($model->supplierId);
 <form class="form" <?php echo ($model->paymentMethod == 1) ? 'action="' . Yii::app()->createUrl("checkout/step/confirmation/id/" . $model->orderGroupId) . '"' : ""; ?>  method="post">
 	<div class="row">
 		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-12 text-center">
-					<h1>ยืนยันการชำระเงิน</h1>
+			<div class="carousel-heading no-margin">
+				<h1>ยืนยันการชำระเงิน</h1>
+			</div>
+
+			<div class="page-content">
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<div class="form-group">
+							<div class="control-label col-md-3">
+								เลขที่ใบเสนอราคา.
+							</div>
+							<div class="col-md-9">
+								<?php echo $model->orderNo; ?>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="control-label col-md-3">
+								ยอดชำระ
+							</div>
+							<div class="col-md-9">
+								<?php echo $model->summary; ?>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="form-group">
-				<div class="control-label col-md-3">
-					เลขที่ใบเสนอราคา.
-				</div>
-				<div class="col-md-9">
-					<?php echo $model->orderNo; ?>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="control-label col-md-3">
-					ยอดชำระ
-				</div>
-				<div class="col-md-9">
-					<?php echo $model->summary; ?>
-				</div>
-			</div>
+
 			<?php
 			echo CHtml::hiddenField("paymentMethod", $model->paymentMethod);
 			if(($model->paymentMethod == 1))
