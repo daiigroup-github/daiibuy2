dc3a92bc15ed153b81e49f4a3a6ce9d1c34c54e6
Kurtumm <kurtumm@gmail.com>
2014-10-29 23:23:00 +0700

    delete alert in daiibuy.js::updateCartHeader

--- js/daiibuy.js
+++ js/daiibuy.js
@@ -15,7 +15,6 @@ $(document).ready(function(){
 });
 
 function updateCartHeader() {
-    alert(baseUrl+'cart/updateCartHeader');
     $.ajax({
         type: 'POST',
         url: baseUrl+'/cart/updateCartHeader',
