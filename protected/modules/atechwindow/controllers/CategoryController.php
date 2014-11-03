29abea4eb68d675faab513bab3aa62d8297ebfa2
kamyjapkung <kamyjap@gmail.com>
2014-11-03 11:10:40 +0700

    atech fornt

--- protected/modules/atechwindow/controllers/CategoryController.php
+++ protected/modules/atechwindow/controllers/CategoryController.php
@@ -2,54 +2,60 @@
 
 class CategoryController extends MasterAtechwindowController
 {
-    public function actionIndex($id)
-    {
-        $colors = array(
-            'ALL',
-            'White',
-            'Brown',
-            'Black',
-            'Gray',
-        );
 
-        $category2 = Category::model()->findByPk($id);
+	public function actionIndex($id)
+	{
+		$colors = array(
+			'ALL',
+			'White',
+			'Brown',
+			'Black',
+			'Gray',
+		);
 
-        $images = [];
-        foreach ($category2->images as $image) {
-            $images[] = Yii::app()->baseUrl . $image->image;
-        }
+		$category2 = Category::model()->findByPk($id);
 
-        $this->render('index', array(
-            'category2'=>$category2,
-            'images' => $images,
-            'colors'=> $colors
-        ));
-    }
+		$images = [];
+		if(count($category2->images) > 0):
+			foreach($category2->images as $image)
+			{
+				$images[] = Yii::app()->baseUrl . $image->image;
+			}
+		else:
+			$images[] = Yii::app()->baseUrl . $category2->image;
+		endif;
 
-    // Uncomment the following methods and override them if needed
-    /*
-    public function filters()
-    {
-        // return the filter configuration for this controller, e.g.:
-        return array(
-            'inlineFilterName',
-            array(
-                'class'=>'path.to.FilterClass',
-                'propertyName'=>'propertyValue',
-            ),
-        );
-    }
+		$this->render('index', array(
+			'category2'=>$category2,
+			'images'=>$images,
+			'colors'=>$colors
+		));
+	}
 
-    public function actions()
-    {
-        // return external action classes, e.g.:
-        return array(
-            'action1'=>'path.to.ActionClass',
-            'action2'=>array(
-                'class'=>'path.to.AnotherActionClass',
-                'propertyName'=>'propertyValue',
-            ),
-        );
-    }
-    */
+	// Uncomment the following methods and override them if needed
+	/*
+	  public function filters()
+	  {
+	  // return the filter configuration for this controller, e.g.:
+	  return array(
+	  'inlineFilterName',
+	  array(
+	  'class'=>'path.to.FilterClass',
+	  'propertyName'=>'propertyValue',
+	  ),
+	  );
+	  }
+
+	  public function actions()
+	  {
+	  // return external action classes, e.g.:
+	  return array(
+	  'action1'=>'path.to.ActionClass',
+	  'action2'=>array(
+	  'class'=>'path.to.AnotherActionClass',
+	  'propertyName'=>'propertyValue',
+	  ),
+	  );
+	  }
+	 */
 }
