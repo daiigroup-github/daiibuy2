00ee45c7adf10db03ef0be88342754d1f273daf5
kamyjapkung <kamyjap@gmail.com>
2014-11-04 11:21:32 +0700

    xx

--- protected/modules/madrid/controllers/CategoryController.php
+++ protected/modules/madrid/controllers/CategoryController.php
@@ -2,53 +2,60 @@
 
 class CategoryController extends MasterMadridController
 {
-    public function actionIndex($id, $id2=null)
-    {
-        $category = Category::model()->findByPk($id);
 
-        //theme
-        $hasTheme = CategoryToSub::model()->find(array(
-            'condition'=>'categoryId=:categoryId AND isTheme=1',
-            'params' => array(
-                ':categoryId'=>$id
-            ),
-        ));
-        if(isset($hasTheme)) {
-            $this->showTheme($id, $id2);
-        }
+	public function actionIndex($id, $id2 = null)
+	{
+		$category = Category::model()->findByPk($id);
 
-        $hasSet = CategoryToSub::model()->find(array(
-            'condition'=>'categoryId=:category AND isSet=1',
-            'params' => array(
-                ':categoryId'=>$id
-            ),
-        ));
-        if(isset($hasSet)) {
-            $this->showSet($category);
-        }
+		//theme
+		$hasTheme = CategoryToSub::model()->find(array(
+			'condition'=>'categoryId=:categoryId AND isTheme=1',
+			'params'=>array(
+				':categoryId'=>$id
+			),
+		));
+		if(isset($hasTheme))
+		{
+			$this->showTheme($id, $id2);
+		}
+
+		$hasSet = CategoryToSub::model()->find(array(
+			'condition'=>'categoryId=:categoryId AND isSet=1',
+			'params'=>array(
+				':categoryId'=>$id
+			),
+		));
+		if(isset($hasSet))
+		{
+			$this->showSet($category);
+		}
 
 
-        if(isset($id2)) {
-            $subCategorysId = $id2;
-        } else {
-            $subCategorys = CHtml::listData($category->subCategorys, 'categoryId', 'categoryId');
-            $subCategorysId = implode(',', $subCategorys);
-        }
+		if(isset($id2))
+		{
+			$subCategorysId = $id2;
+		}
+		else
+		{
+			$subCategorys = CHtml::listData($category->subCategorys, 'categoryId', 'categoryId');
+			$subCategorysId = implode(',', $subCategorys);
+		}
 
-        $category2ToProducts = Category2ToProduct::model()->findAll(array(
-            'condition'=>'category1Id=:category1Id AND category2Id IN (:category2Id)',
-            'params' => array(
-                ':category1Id'=>$id,
-                ':category2Id'=>$subCategorysId,
-            ),
-        ));
+		$category2ToProducts = Category2ToProduct::model()->findAll(array(
+			'condition'=>'category1Id=:category1Id AND category2Id IN (:category2Id)',
+			'params'=>array(
+				':category1Id'=>$id,
+				':category2Id'=>$subCategorysId,
+			),
+		));
 
-        $criteria = new CDbCriteria();
-        $criteria->addInCondition('productId', CHtml::listData($category2ToProducts, 'productId', 'productId'));
+		$criteria = new CDbCriteria();
+		$criteria->addInCondition('productId', CHtml::listData($category2ToProducts, 'productId', 'productId'));
 
-        $dataProvider = new CActiveDataProvider('Product', array('criteria'=>$criteria));
-        $dataProvider->pagination->pageSize = 9;
-        $template = "<div class='row'>
+		$dataProvider = new CActiveDataProvider('Product', array(
+			'criteria'=>$criteria));
+		$dataProvider->pagination->pageSize = 9;
+		$template = "<div class='row'>
 			<div class='col-lg-4 col-md-4 col-sm-4'>{summary}</div>\n
 			<div class='col-lg-8 col-md-8 col-sm-8'>{pager}</div>\n
 		</div>
@@ -59,43 +66,43 @@ class CategoryController extends MasterMadridController
 			<div class='col-lg-4 col-md-4 col-sm-4'>{summary}</div>\n
 			<div class='col-lg-8 col-md-8 col-sm-8'>{pager}</div>\n
 		</div>";
-        $summaryText = '<p>Display {start}-{end} of {count} items (page {page} of {pages})</p>';
+		$summaryText = '<p>Display {start}-{end} of {count} items (page {page} of {pages})</p>';
 
-        $this->sideBarCategories = $this->sideBarCategories($id);
+		$this->sideBarCategories = $this->sideBarCategories($id);
 
-        $this->render('index', array(
-            'title' => $category->title,
-            'dataProvider' => $dataProvider,
-            'itemView' => '_product_item',
-            'template' => $template,
-            'summaryText' => $summaryText,
-        ));
-    }
+		$this->render('index', array(
+			'title'=>$category->title,
+			'dataProvider'=>$dataProvider,
+			'itemView'=>'_product_item',
+			'template'=>$template,
+			'summaryText'=>$summaryText,
+		));
+	}
 
-    // Uncomment the following methods and override them if needed
-    /*
-    public function filters()
-    {
-        // return the filter configuration for this controller, e.g.:
-        return array(
-            'inlineFilterName',
-            array(
-                'class'=>'path.to.FilterClass',
-                'propertyName'=>'propertyValue',
-            ),
-        );
-    }
+	// Uncomment the following methods and override them if needed
+	/*
+	  public function filters()
+	  {
+	  // return the filter configuration for this controller, e.g.:
+	  return array(
+	  'inlineFilterName',
+	  array(
+	  'class'=>'path.to.FilterClass',
+	  'propertyName'=>'propertyValue',
+	  ),
+	  );
+	  }
 
-    public function actions()
-    {
-        // return external action classes, e.g.:
-        return array(
-            'action1'=>'path.to.ActionClass',
-            'action2'=>array(
-                'class'=>'path.to.AnotherActionClass',
-                'propertyName'=>'propertyValue',
-            ),
-        );
-    }
-    */
+	  public function actions()
+	  {
+	  // return external action classes, e.g.:
+	  return array(
+	  'action1'=>'path.to.ActionClass',
+	  'action2'=>array(
+	  'class'=>'path.to.AnotherActionClass',
+	  'propertyName'=>'propertyValue',
+	  ),
+	  );
+	  }
+	 */
 }
