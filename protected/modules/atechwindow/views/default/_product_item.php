5f6bb7bd727200609c340e95e420533bd25e5365
kamyjapkung <kamyjap@gmail.com>
2014-11-06 11:39:04 +0700

    fix link image atech

--- protected/modules/atechwindow/views/default/_product_item.php
+++ protected/modules/atechwindow/views/default/_product_item.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="grid-view product">
         <div class="product-image col-lg-4 col-md-4 col-sm-4">
-			<?php echo CHtml::link(CHtml::image(Yii::app()->createUrl($category->image), $category->title), $this->createUrl('category/index/id/' . $category->categoryId)); ?>
+			<?php echo CHtml::link(CHtml::image(Yii::app()->createUrl($category->image), $category->title), $this->createUrl('category/index?id=' . $category->categoryId . (isset($category1Id) ? "&category1Id=" . $category1Id : ""))); ?>
         </div>
 
         <div class="col-lg-8 col-md-8 col-sm-8 product-content no-padding" style="height: 290px;">
