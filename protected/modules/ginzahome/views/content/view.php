73ce31efa839a6a8b76f8a3066da1dacc898facb
Kurtumm <kurtumm@gmail.com>
2014-11-11 11:32:49 +0700

    Content :: view

--- protected/modules/ginzahome/views/content/view.php
+++ protected/modules/ginzahome/views/content/view.php
@@ -1,15 +1,18 @@
 <div class="row">
-	<div class="col-md-12">
-		<?php echo $model->title; ?>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-12">
-		<?php echo CHtml::image(Yii::app()->baseUrl . $model->image, $model->title); ?>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-12">
-		<?php echo $model->description; ?>
-	</div>
+
+    <div class="col-lg-12 col-md-12 col-sm-12">
+
+        <div class="carousel-heading no-margin">
+            <h4><?php echo $model->title; ?></h4>
+        </div>
+
+        <div class="page-content">
+            <?php if(isset($model->image)):?>
+                <p><?php echo CHtml::image(Yii::app()->baseUrl . $model->image, $model->title); ?></p>
+            <?php endif;?>
+            <?php echo $model->description; ?>
+        </div>
+
+    </div>
+
 </div>
