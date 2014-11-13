03a3fef27a34d0c1533ce0a10759d6f1f3d38f79
kamyjapkung <kamyjap@gmail.com>
2014-11-13 10:58:48 +0700

    xx

--- protected/modules/madrid/views/category/_product_item.php
+++ protected/modules/madrid/views/category/_product_item.php
@@ -1,25 +1,27 @@
 <?php
 /*
-$data = [
-	'id'=>'',
-	'image'=>'',
-    'url'=>'',
-    'title'=>'',
-    'price'=>'',
-	'pricePromotion'=>'',
-    'buttons'=>[
-	    'cart',
-	    'favorites',
-	    'compare',
-    ],
-];
-*/
+  $data = [
+  'id'=>'',
+  'image'=>'',
+  'url'=>'',
+  'title'=>'',
+  'price'=>'',
+  'pricePromotion'=>'',
+  'buttons'=>[
+  'cart',
+  'favorites',
+  'compare',
+  ],
+  ];
+ */
 $image = '';
-if (isset($data->productImages)) {
-    foreach ($data->productImages as $productImage) {
-        $image = $productImage->image;
-        break;
-    }
+if(isset($data->productImages))
+{
+	foreach($data->productImages as $productImage)
+	{
+		$image = $productImage->image;
+		break;
+	}
 }
 ?>
 <!-- Product Item -->
@@ -27,29 +29,29 @@ if (isset($data->productImages)) {
     class="<?php echo ($this->id == 'category') ? 'col-lg-4 col-md-4 col-sm-4' : 'col-lg-3 col-md-3 col-sm-4'; ?> product">
 
     <div class="product-image">
-        <?php if (isset($data->productPromotion)): ?>
-            <span class="product-tag">Sale</span>
-        <?php endif; ?>
-        <?php echo CHtml::image(Yii::app()->baseUrl . $image); ?>
-        <?php /*
-            <a href="<?php echo $this->createUrl('product/index/id/'.$data['productId']); ?>" class="product-hover">
-                <i class="icons icon-eye-1"></i> Quick View
-            </a>
-        */
-        ?>
+		<?php if(isset($data->productPromotion)): ?>
+			<span class="product-tag">Sale</span>
+		<?php endif; ?>
+		<?php echo CHtml::image(Yii::app()->baseUrl . $image); ?>
+		<?php /*
+		  <a href="<?php echo $this->createUrl('product/index/id/'.$data['productId']); ?>" class="product-hover">
+		  <i class="icons icon-eye-1"></i> Quick View
+		  </a>
+		 */
+		?>
     </div>
 
     <div class="product-info">
         <h5>
             <a href="<?php echo $this->createUrl('product/index/id/' . $data['productId']); ?>"><?php echo mb_substr($data['name'], 0, 29, 'utf8'); ?></a>
         </h5>
-        <?php if (isset($data['price'])): ?>
-            <?php if (isset($data->productPromotion)): ?>
-                <span class="price"><del><?php echo number_format($data->price, 2); ?></del><?php echo number_format($data->productPromotion->price, 2); ?></span>
-            <?php else: ?>
-                <span class="price"><?php echo number_format($data->price, 2); ?></span>
-            <?php endif; ?>
-        <?php endif; ?>
+		<?php if(isset($data['price'])): ?>
+			<?php if(isset($data->productPromotion)): ?>
+				<span class="price"><del><?php echo number_format($data->price, 2); ?></del><?php echo number_format($data->productPromotion->price, 2); ?></span>
+			<?php else: ?>
+				<span class="price">ราคา <?php echo number_format($data->price, 2); ?> บาท</span>
+	<?php endif; ?>
+<?php endif; ?>
     </div>
 
     <div class="product-actions">
