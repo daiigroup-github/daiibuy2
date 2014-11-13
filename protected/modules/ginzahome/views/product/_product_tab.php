a4b02b364ab7fde7aae9b25ae60f82ea30e37203
kamyjapkung <kamyjap@gmail.com>
2014-11-13 12:13:49 +0700

    xx

--- protected/modules/ginzahome/views/product/_product_tab.php
+++ protected/modules/ginzahome/views/product/_product_tab.php
@@ -34,36 +34,39 @@ $tabContent = '';
 					?>
 					<div id="<?php echo $tabId ?>">
 						<?php echo $tab['detail']; ?>
-					</div>
-					<?php
-					$detailChilds = ProductSpecGroup::model()->findAll("parentId = " . $tab["id"]);
-					foreach($detailChilds as $child):
-						?>
-						<div class="row">
-							<div class="col-lg-12 col-md-12 col-sm-12">
 
-								<div class="carousel-heading no-margin">
-									<h4><?php echo $child->title; ?></h4>
-								</div>
+						<?php
+						$detailChilds = ProductSpecGroup::model()->findAll("parentId = " . $tab["id"]);
+						foreach($detailChilds as $child):
+							?>
+							<div class="row">
+								<div class="col-lg-12 col-md-12 col-sm-12">
 
-								<div class="page-content">
-									<?php foreach($child->productSpecs as $item): ?>
-										<?php if(isset($item->image)): ?>
-											<p><?php
-												echo CHtml::image(Yii::app()->baseUrl . $item->image, $item->title, array(
-													'class'=>isset($item->spanWidth) ? "col-md=" . $item->spanWidth : ""));
-												?></p>
-										<?php endif; ?>
-										<?php echo $item->description; ?>
-									<?php endforeach; ?>
+									<div class="carousel-heading no-margin">
+										<h4><?php echo $child->title; ?></h4>
+									</div>
+
+									<div class="page-content">
+										<?php foreach($child->productSpecs as $item): ?>
+											<?php if(isset($item->image)): ?>
+												<p><?php
+													echo CHtml::image(Yii::app()->baseUrl . $item->image, $item->title, array(
+														'class'=>isset($item->spanWidth) ? "col-md=" . $item->spanWidth : ""));
+													?></p>
+											<?php endif; ?>
+											<?php echo $item->description . "<br>"; ?>
+										<?php endforeach; ?>
+									</div>
+
 								</div>
 
 							</div>
-
-						</div>
-						<?php
-					endforeach;
-					$i++;
+							<?php
+						endforeach;
+						$i++;
+						?>
+					</div>
+					<?php
 				}
 				?>
             </div>
