68fb6ff4a02a5459a09cc6a80eebfb2db5c9eedb
kamyjapkung <kamyjap@gmail.com>
2014-11-13 16:57:46 +0700

    xx

--- protected/modules/myfile/views/ginzaHome/index.php
+++ protected/modules/myfile/views/ginzaHome/index.php
@@ -15,7 +15,7 @@ $this->breadcrumbs = array(
 					<?php
 					echo CHtml::image(Yii::app()->baseUrl . $myfile->orders[0]->orderItems[0]->product->image, "", array())
 					?>
-					<a class="btn <?php echo ($myfile->status == 3) ? "btn-success" : "btn-primary" ?> col-md-12"  href="<?php echo Yii::app()->createUrl('/index.php/myfile/ginzahome/view/id/' . $myfile->orderGroupId); ?>">
+					<a class="btn <?php echo ($myfile->status == 3) ? "btn-success" : "btn-primary" ?> col-md-12"  href="<?php echo Yii::app()->createUrl('/index.php/myfile/ginzaHome/view/id/' . $myfile->orderGroupId); ?>">
 						<h3><?php echo $myfile->orderNo; ?><?php if($myfile->status == 3): ?><i class="fa fa-comments pull-left"></i><?php endif; ?></h3>
 					</a>
 				</div>
