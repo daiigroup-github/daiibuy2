2a799fb9490d23229d76a428bde51d1a2513b14a
kamyjapkung <kamyjap@gmail.com>
2014-11-13 22:40:41 +0700

    myfile view height

--- protected/modules/myfile/views/atechWindow/index.php
+++ protected/modules/myfile/views/atechWindow/index.php
@@ -29,7 +29,7 @@ $this->breadcrumbs = array(
 		<?php foreach($myfileArray as $myfile): ?>
 			<div class='col-lg-3 col-md-3 col-sm-12'>
 				<div class="blog-item">
-					<a style="height:100px;" class="btn <?php echo ($myfile->status == 1) ? "btn-success" : "btn-primary" ?> col-md-12"  href="<?php echo Yii::app()->createUrl('/index.php/myfile/atechWindow/view/id/' . $myfile->orderId); ?>">
+					<a style="" class="btn <?php echo ($myfile->status == 1) ? "btn-success" : "btn-primary" ?> col-md-12"  href="<?php echo Yii::app()->createUrl('/index.php/myfile/atechWindow/view/id/' . $myfile->orderId); ?>">
 						<h3><?php echo $myfile->title; ?><?php if($myfile->status == 1): ?><i class="fa fa-comments pull-left"></i><?php endif; ?></h3>
 						<p>วันที่สร้าง :<?php echo $this->dateThai($myfile->createDateTime, 3, TRUE); ?></p>
 						<p>วันที่แก้ไข :<?php echo $this->dateThai($myfile->updateDateTime, 2, TRUE) ?></p>
