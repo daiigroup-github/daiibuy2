924456a9f5acb4ebb4f84b213f81e2776c6eef29
kamyjapkung <kamyjap@gmail.com>
2014-11-13 09:23:48 +0700

    xx

--- protected/modules/atechwindow/views/category/_product_list.php
+++ protected/modules/atechwindow/views/category/_product_list.php
@@ -6,13 +6,15 @@ foreach($models as $model):
 		<div class="grid-view product">
 			<div class="product-image col-lg-2 col-md-2 col-sm-2">
 				<?php
-				echo CHtml::link(isset($model->product->productImages[0]) ? CHtml::image(Yii::app()->createUrl($model->product->productImages[0]->image), $model->product->name, array(
-							'style'=>'width:150px')) : "", $this->createUrl('category/index?id=' . $model->category2Id . (isset($category1Id) ? "&category1Id=" . $model->category1Id : "")));
+//				echo CHtml::link(isset($model->product->productImages[0]) ? CHtml::image(Yii::app()->createUrl($model->product->productImages[0]->image), $model->product->name, array(
+//							'style'=>'width:150px')) : "", $this->createUrl('category/index?id=' . $model->category2Id . (isset($category1Id) ? "&category1Id=" . $model->category1Id : "")));
+				echo isset($model->product->productImages[0]) ? CHtml::image(Yii::app()->createUrl($model->product->productImages[0]->image), $model->product->name, array(
+						'style'=>'width:150px')) : "";
 				?>
 			</div>
 			<div class="col-lg-5 col-md-5 col-sm-5 product-content no-padding" style="height: 290px;">
 				<div class="product-info">
-					<h5><?php echo CHtml::link($model->product->name, $this->createUrl('category/index?id=' . $model->category2Id . (isset($category1Id) ? "&category1Id=" . $model->category1Id : ""))); ?></h5>
+					<h5><?php echo $model->product->name; ?></h5>
 					<p><?php echo $model->product->description; ?></p>
 				</div>
 			</div>
