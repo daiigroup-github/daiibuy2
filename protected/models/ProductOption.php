924a7a047dc3cc2e78f536d224cef9f13ab7c1b6
kamyjapkung <kamyjap@gmail.com>
2014-11-17 11:28:49 +0700

    product optoion search

--- protected/models/ProductOption.php
+++ protected/models/ProductOption.php
@@ -2,54 +2,90 @@
 
 class ProductOption extends ProductOptionMaster
 {
-    /**
-     * Returns the static model of the specified AR class.
-     * @param string $className active record class name.
-     * @return Product the static model class
-     */
 
-    public static function model($className = __CLASS__)
-    {
-        return parent::model($className);
-    }
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Product the static model class
+	 */
+	public static function model($className = __CLASS__)
+	{
+		return parent::model($className);
+	}
 
-    /**
-     * @return array validation rules for model attributes.
-     */
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		return CMap::mergeArray(parent::rules(), array(
+				//code here
+		));
+	}
 
-    public function rules()
-    {
-        return CMap::mergeArray(parent::rules(), array(//code here
-        ));
-    }
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return CMap::mergeArray(parent::relations(), array(
+				//code here
+		));
+	}
 
-    /**
-     * @return array relational rules.
-     */
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return Cmap::mergeArray(parent::attributeLabels(), array(
+				//code here
+		));
+	}
 
-    public function relations()
-    {
-        // NOTE: you may need to adjust the relation name and the related
-        // class name for the relations automatically generated below.
-        return CMap::mergeArray(parent::relations(), array(//code here
-        ));
-    }
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 * public function search()
+	 * {
+	 * }
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
 
-    /**
-     * @return array customized attribute labels (name=>label)
-     */
+		$criteria = new CDbCriteria;
 
-    public function attributeLabels()
-    {
-        return Cmap::mergeArray(parent::attributeLabels(), array(//code here
-        ));
-    }
+		if(isset($this->searchText) && !empty($this->searchText))
+		{
+			$this->productOptionId = $this->searchText;
+			$this->productOptionGroupId = $this->searchText;
+			$this->title = $this->searchText;
+			$this->description = $this->searchText;
+			$this->image = $this->searchText;
+			$this->priceValue = $this->searchText;
+			$this->pricePercent = $this->searchText;
+			$this->status = $this->searchText;
+			$this->createDateTime = $this->searchText;
+			$this->updateDateTime = $this->searchText;
+		}
 
-    /**
-     * Retrieves a list of models based on the current search/filter conditions.
-     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-     * public function search()
-     * {
-     * }
-     */
+		$criteria->compare('productOptionId', $this->productOptionId, true, 'OR');
+		$criteria->compare('productOptionGroupId', $this->productOptionGroupId);
+		$criteria->compare('title', $this->title, true, 'OR');
+		$criteria->compare('description', $this->description, true, 'OR');
+		$criteria->compare('image', $this->image, true, 'OR');
+		$criteria->compare('priceValue', $this->priceValue, true, 'OR');
+		$criteria->compare('pricePercent', $this->pricePercent, true, 'OR');
+		$criteria->compare('status', $this->status);
+		$criteria->compare('createDateTime', $this->createDateTime, true, 'OR');
+		$criteria->compare('updateDateTime', $this->updateDateTime, true, 'OR');
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
 }
