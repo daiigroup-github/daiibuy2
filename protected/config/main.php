068ac8dee84e1bcef5be961708c02ac3045a164f
xaqu <chilloutto@gmail.com>
2014-11-18 13:00:06 +0700

    mail config update
    sync

--- protected/config/main.php
+++ protected/config/main.php
@@ -90,7 +90,7 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
-		'sendEmail'=>FALSE,
+		'sendEmail'=>true,
 		'ePaymentServerType'=>1
 	),
 );
