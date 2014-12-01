766ccf2d6c6cbd758df5fdcb755040a451287fe3
xaqu <chilloutto@gmail.com>
2014-12-01 16:31:30 +0700

    step fixed change province in checkout step
    sync

--- protected/controllers/SelectProvinceController.php
+++ protected/controllers/SelectProvinceController.php
@@ -5,15 +5,17 @@ class SelectProvinceController extends MasterController
 
 	public function actionSaveProvince()
 	{
-		if(isset($_POST['provinceId']))
-		{
-			$this->cookie = new DaiiBuy();
-			$this->cookie->loadCookie();
-
-			$this->cookie->provinceId = $_POST['provinceId'];
-			$this->cookie->saveCookie();
-
-			echo json_encode($this->cookie);
+			if(isset($_POST['provinceId']))
+			{
+				if(($this->action->id == 'step') && isset(Yii::app()->user->id)){
+					$this->redirect(Yii::app()->baseUrl . "/checkout/step/2");
+				}else{
+					$this->cookie = new DaiiBuy();
+					$this->cookie->loadCookie();
+					$this->cookie->provinceId = $_POST['provinceId'];
+					$this->cookie->saveCookie();
+				echo json_encode($this->cookie);
+			}
 		}
 	}
 }
