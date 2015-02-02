d0995e818bc02a32f0141a3fa2cf390c0b1963da
kamyjapkung <kamyjap@gmail.com>
2015-02-02 15:37:03 +0700

    xx

--- protected/modules/myfile/views/ginzaHome/_wizard_step.php
+++ protected/modules/myfile/views/ginzaHome/_wizard_step.php
@@ -1,7 +1,7 @@
 <div class="row form-group hidden">
 	<div class="col-xs-12">
 		<ul class="nav nav-pills nav-justified thumbnail setup-panel">
-			<li class="<?php echo ($this->action->id == 'view' && ($model->status == 3)) ? 'active' : ''; ?>"><a href="#step-3">
+			<li class="<?php echo ($this->action->id == 'view' && ($model->status >= 3 )) ? 'active' : ''; ?>"><a href="#step-3">
 					<h4 class="list-group-item-heading">Step 3</h4>
 					<p class="list-group-item-text">เปรียบเทียบประเมินราคา</p>
 				</a></li>
