60ce014e57d2379495758dba546befb77bcd9ec1
kamyjapkung <kamyjap@gmail.com>
2015-08-25 09:54:02 +0700

    Fix Atech Product Slider

--- themes/homeshop/views/layouts/_product_slider.php
+++ themes/homeshop/views/layouts/_product_slider.php
@@ -1,36 +1,36 @@
 <div id="product-slider">
     <ul class="slides">
         <li>
-            <?php if (isset($images)): ?>
-                <img class="cloud-zoom" src="<?php echo isset($images->image) ? Yii::app()->createUrl($images->image) : (isset($images[0]) ? $images[0] : "" ); ?>" data-large="<?php echo isset($images->image) ? Yii::app()->createUrl($images->image) : (isset($images[0]) ? $images[0] : "" ); ?>" alt="" />
-                <a class="fullscreen-button" href="<?php echo isset($images->image) ? Yii::app()->createUrl($images->image) : (isset($images[0]) ? $images[0] : "" ); ?>">
-                    <div class="product-fullscreen">
-                        <i class="icons icon-resize-full-1"></i>
-                    </div>
-                </a>
-                <?php
-            endif;
-            ?>
+			<?php if(isset($images)): ?>
+				<img class="cloud-zoom" src="<?php echo isset($images->image) ? Yii::app()->createUrl($images->image) : (isset($images[0]) ? $images[0] : "" ); ?>" data-large="<?php echo isset($images->image) ? Yii::app()->createUrl($images->image) : (isset($images[0]) ? $images[0] : "" ); ?>" alt="" />
+				<a class="fullscreen-button" href="<?php echo isset($images->image) ? Yii::app()->createUrl($images->image) : (isset($images[0]) ? $images[0] : "" ); ?>">
+					<div class="product-fullscreen">
+						<i class="icons icon-resize-full-1"></i>
+					</div>
+				</a>
+				<?php
+			endif;
+			?>
         </li>
     </ul>
 </div>
 <div id="product-carousel">
     <ul class="slides">
-<?php foreach ($images as $id => $image): ?>
-            <li>
-                <a class="fancybox" rel="product-images" href="<?php echo $image;  ?>"></a>
-                <img src="<?php echo $image;  ?>" data-large="<?php echo $image;  ?>" alt="" id="imageThumbnail<?php echo $id;  ?>" />
-            </li>
-<?php endforeach; ?>
+		<?php foreach($images->images as $id=> $image): ?>
+			<li>
+				<a class="fancybox" rel="product-images" href="<?php echo $image; ?>"></a>
+				<img src="<?php echo $image; ?>" data-large="<?php echo $image; ?>" alt="" id="imageThumbnail<?php echo $id; ?>" />
+			</li>
+		<?php endforeach; ?>
     </ul>
-<?php if (sizeof($images) > 4): ?>
-        <div class="product-arrows">
-            <div class="left-arrow">
-                <i class="icons icon-left-dir"></i>
-            </div>
-            <div class="right-arrow">
-                <i class="icons icon-right-dir"></i>
-            </div>
-        </div>
-<?php endif; ?>
+	<?php if(sizeof($images->images) > 4): ?>
+		<div class="product-arrows">
+			<div class="left-arrow">
+				<i class="icons icon-left-dir"></i>
+			</div>
+			<div class="right-arrow">
+				<i class="icons icon-right-dir"></i>
+			</div>
+		</div>
+	<?php endif; ?>
 </div>
