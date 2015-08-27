d93e84ddd474772cbe4587fd4c5da6a01c99cef6
Kurtumm <kurtumm@gmail.com>
2015-08-27 16:44:34 +0700

    fixed atechwindow

--- protected/modules/atechwindow/controllers/DefaultController.php
+++ protected/modules/atechwindow/controllers/DefaultController.php
@@ -33,25 +33,14 @@ class DefaultController extends MasterAtechwindowController {
             $brandId = $brands[0]->brandId;
         }
 
-        $category2ToProducts = Category2ToProduct::model()->findAll('brandId = ' . $brandId . ' AND status = 1 group by category1Id');
+        $criteria = new CDbCriteria();
+        $criteria->with = 'category';
+        $criteria->condition = 't.brandId=:brandId AND t.status=1 AND category.status=1';
+        $criteria->params = array(':brandId'=>$brandId);
+        $criteria->group = 't.category1Id';
+        $category2ToProducts = Category2ToProduct::model()->findAll($criteria);
 
         $categoryId = isset($categoryId) ? $categoryId : $category2ToProducts[0]->category->categoryId;
-
-        /*
-        if (isset($category2ToProducts[0]))
-            $defaultCategory2 = Category::model()->findByPk($category2ToProducts[0]->category2Id);
-
-        $images = [];
-        if (isset($defaultCategory2->images)):
-            if (count($defaultCategory2->images) > 0):
-                foreach ($defaultCategory2->images as $image) {
-                    $images[] = Yii::app()->baseUrl . $image->image;
-                }
-            else:
-                $images[] = Yii::app()->baseUrl . $defaultCategory2->image;
-            endif;
-        endif;
-        */
 
         $buttons = [];
         $k = 0;
@@ -73,8 +62,6 @@ class DefaultController extends MasterAtechwindowController {
         $this->render('index', array(
             'category2ToProducts' => $category2ToProducts,
             'category2ToProduct' => $c2tp,
-//            'images' => $images,
-//            'category2Id' => isset($defaultCategory2->categoryId) ? $defaultCategory2->categoryId : null,
             'colors' => $colors,
             'buttons'=>$buttons,
             'brandId'=>$brandId
