c70f0dc4569e86bec0dfeabd225e2177ab2b6d99
Kurtumm <kurtumm@gmail.com>
2015-08-27 14:18:21 +0700

    change atechwindow from tab to link

--- protected/modules/atechwindow/controllers/DefaultController.php
+++ protected/modules/atechwindow/controllers/DefaultController.php
@@ -4,7 +4,7 @@ class DefaultController extends MasterAtechwindowController {
 
 //    public $layout = '//layouts/cl1';
 
-    public function actionIndex() {
+    public function actionIndex($brandId=null, $categoryId=null) {
         $colors = array(
             'ALL',
             'White',
@@ -12,33 +12,7 @@ class DefaultController extends MasterAtechwindowController {
             'Black',
             'Gray',
         );
-//        $category1 = Category::model()->findByPk($category1Id);
-//        if ($category1->subCategorys !== array()) {
-//            $dropdown = array();
-//            $j = 0;
-//            foreach ($category->subCategorys as $subCategory) {
-//                $dropdown[$j] = array(
-//                    'url' => $this->createUrl('category/index?id=' . $subCategory->categoryId . "&category1Id=" . $category->categoryId),
-//                    'caption' => strtoupper($subCategory->title),
-//                );
-//                $j++;
-//            }
-//            $this->nav[$i]['dropdown'] = $dropdown;
-//        }
-        //Tong Loop For Show Side Category By Cat1
-//        $catToSubs = CategoryToSub::model()->findAll("categoryId=" . $_GET["category2Id"] . " GROUP By subCategoryId");
-//        $i = 0;
-//        $sideBar = array();
-//        foreach ($catToSubs as $catToSub) {
-//            $sideBar[$i] = array(
-//                'url' => $this->createUrl('category/index?id=' . $catToSub->subCategoryId . "&category1Id=" . $catToSub->categoryId),
-//                'link' => strtoupper($catToSub->subCategory->title),
-//            );
-//            $i++;
-//        }
-//        $this->sideBarCategories = array(
-//            'title' => 'Atech Window',
-//            'items' => $sideBar);
+
         //Tong Loop For Show Side Category By Cat1
         $supplier = Supplier::model()->find(array(
             'condition' => 'url=:url',
@@ -46,34 +20,27 @@ class DefaultController extends MasterAtechwindowController {
                 ':url' => $this->module->id,
             ),
         ));
-        $brands = Brand::model()->findAll(array(
-            'condition' => 'supplierId=:supplierId',
-            'params' => array(
-                ':supplierId' => $supplier->supplierId,
-            ),
-            'order' => 'title ASC',
+
+        if(!isset($brandId)) {
+            $brands = Brand::model()->findAll(array(
+                'condition' => 'supplierId=:supplierId',
+                'params' => array(
+                    ':supplierId' => $supplier->supplierId,
+                ),
+                'order' => 'title ASC',
 //            'group by' => 'category2Id',
-        ));
-//        throw new Exception(print_r($brands, true));
-        if (isset($_GET["brandId"]))
-            $brandId = $_GET["brandId"];
-        if (!isset($brandId))
+            ));
             $brandId = $brands[0]->brandId;
-
-
+        }
 
         $category2ToProducts = Category2ToProduct::model()->findAll('brandId = ' . $brandId . ' AND status = 1 group by category1Id');
-//        throw new Exception(print_r($category2ToProducts[0]->category2Id, true));
+
+        $categoryId = isset($categoryId) ? $categoryId : $category2ToProducts[0]->category->categoryId;
+
+        /*
         if (isset($category2ToProducts[0]))
             $defaultCategory2 = Category::model()->findByPk($category2ToProducts[0]->category2Id);
-//        throw new Exception(print_r(count($category2ToProducts), true));
-//        if (isset($_GET[""])) {
-//            $category2Id = $_GET["category2Id"];
-//            $defaultCategory2 = Category::model()->findByPk($category2Id);
-//        } else {
-//            $defaultCategory2 = $category1->subCategorys[0];
-//            $category2Id = $defaultCategory2->categoryId;
-//        }
+
         $images = [];
         if (isset($defaultCategory2->images)):
             if (count($defaultCategory2->images) > 0):
@@ -84,73 +51,35 @@ class DefaultController extends MasterAtechwindowController {
                 $images[] = Yii::app()->baseUrl . $defaultCategory2->image;
             endif;
         endif;
+        */
 
-//        throw new Exception(print_r($brandId, true));
-        //Create By Tong
-//        $widthArray = Product::model()->findAtechWidthGroup($id);
-//        $heightArray = Product::model()->findAtechHeightGroup($id);
+        $buttons = [];
+        $k = 0;
+        foreach ($category2ToProducts as $category2ToProduct) {
+            if($categoryId == $category2ToProduct->category->categoryId){
+                $active = 'active';
+                $c2tp = $category2ToProduct;
+            } else {
+                $active = '';
+            }
+
+            $buttons[$k] = CHtml::link($category2ToProduct->category->title,
+                $this->createUrl('default/index/brandId/'.$brandId.'/categoryId/'.$category2ToProduct->category->categoryId),
+                array('class'=>'button small '.$active));
+            $k++;
+        }
+
 
         $this->render('index', array(
             'category2ToProducts' => $category2ToProducts,
-            'images' => $images,
-            'category2Id' => isset($defaultCategory2->categoryId) ? $defaultCategory2->categoryId : null,
+            'category2ToProduct' => $c2tp,
+//            'images' => $images,
+//            'category2Id' => isset($defaultCategory2->categoryId) ? $defaultCategory2->categoryId : null,
             'colors' => $colors,
-//            'colors' => $colors,
-//            'widthArray' => $widthArray,
-//            'heightArray' => $heightArray
+            'buttons'=>$buttons,
+            'brandId'=>$brandId
         ));
-//		$title = 'Fenzer';
-//
-//		//pager
-//		$items = $this->showCategory();
-//
-//		$dataProvider = new CArrayDataProvider($items, array(
-//			'keyField'=>'id'));
-//		$template = "<div class='row'>
-//			<div class='col-lg-6 col-md-6 col-sm-6'>{summary}</div>\n
-//			<div class='col-lg-6 col-md-6 col-sm-6'>{pager}</div>\n
-//		</div>
-//		<div class='row'>
-//			{items}
-//		</div>
-//		<div class='row'>
-//			<div class='col-lg-6 col-md-6 col-sm-6'>{summary}</div>\n
-//			<div class='col-lg-6 col-md-6 col-sm-6'>{pager}</div>\n
-//		</div>";
-//
-//		$data = array();
-//
-//		$supplierModel = Supplier::model()->find(array(
-//			'condition'=>'url=:url',
-//			'params'=>array(
-//				':url'=>$this->module->id)));
-//		if(!isset($id))
-//		{
-//			$categorys = Category::model()->findAll(array(
-//				'condition'=>'supplierId=:supplierId AND isRoot=0 AND status=1',
-//				'params'=>array(
-//					':supplierId'=>$supplierModel->supplierId)));
-//		}
-//		else
-//		{
-//			$category = Category::model()->findByPk($id);
-//			$categorys = $category->subCategorys;
-//		}
-//
-//		/*
-//		  $this->render('index', array(
-//		  'title' => $title,
-//		  'dataProvider' => $dataProvider,
-//		  'itemView' => '//layouts/_product_item2',
-//		  'template' => $template,
-//		  'items' => $items,
-//		  ));
-//		 */
-//
-//		$this->render('index', array(
-//			'supplierModel'=>$supplierModel,
-//			'categorys'=>$categorys,
-//			'category1Id'=>isset($category) ? $category->categoryId : NULL));
+
     }
 
 }
--- protected/modules/atechwindow/views/default/index.php
+++ protected/modules/atechwindow/views/default/index.php
@@ -5,26 +5,37 @@ $this->breadcrumbs = array(
 ?>
 
 <div class="row">
+    <div class="col-md-12">
+        <div class="tab-heading">
+            <?php foreach ($buttons as $button) {
+                echo $button;
+            }
+            ?>
+        </div>
+
+        <div class="page-content tab-content">
+            <?php $this->renderPartial('//layouts/_product_single_atech', array('subCate' => $category2ToProduct->category, 'colors' => $colors, 'brandId' => $brandId)); ?>
+        </div>
+    </div>
+</div>
+
+<?php /*
+<div class="row">
     <div style="">
         <?php if (count($category2ToProducts) > 0) { ?>
             <div class="tabs">
 
                 <div class="tab-heading">
                     <?php
-//                $i = 1;
                     foreach ($category2ToProducts as $cate2ToProduct) {
 
                         $cate1h = Category::model()->findByPk($cate2ToProduct->category1Id);
                         if (isset($cate1h)):
-//                    throw new Exception(print_r($cate2h->title . ", " . $cate2h->categoryId, true));
-//                    echo $cate2->title;
                             echo CHtml::link($cate1h->title . ' ', '#' . $cate1h->categoryId, array(
                                 'class' => 'button small'));
                             ?>
                             <?php
-//                    $i++;
-//                    echo CHtml::link("History" . ' ', '#' . 2, array(
-//                        'class' => 'button big'));
+/
                         endif;
                     }
                     ?>
@@ -34,55 +45,16 @@ $this->breadcrumbs = array(
                     foreach ($category2ToProducts as $cate2ToProduct) {
 
                         $cate1c = Category::model()->findByPk($cate2ToProduct->category1Id);
-//                    throw new Exception(print_r($cate2c->categoryId, true));
                         if (isset($cate1c)):
                             ?>
                             <div id="<?php echo $cate1c->categoryId; ?>" class="row-fluid">
-                                <?php // $i = 0;     ?>
-                                <?php // foreach($myfileArray as $myfile):   ?>
-                                <!--<div class='col-lg-3 col-md-3 col-sm-12'>-->
                                 <?php $this->renderPartial('//layouts/_product_single_atech', array('subCate' => $cate1c, 'colors' => $colors, 'brandId' => $cate2ToProduct->brandId)); ?>
-                                <!--</div>-->
-                                <!--<div class='col-lg-12'>-->
-                                <?php // echo $subCate->title;     ?>
-                                <!--                        <div class="blog-item">
-                                                            <a class="btn <?php // echo ($myfile->status == 1) ? "btn-success" : "btn-primary"                                                                                                                  ?> col-md-12"  href="<?php // echo Yii::app()->createUrl('/index.php/myfile/madrid/view/id/' . $myfile->orderId);                                                                                                                  ?>">
-                                                                <h3><?php // echo $myfile->title;                                                                                                                ?><?php // if($myfile->status == 1):                                                                                                                ?><i class="fa fa-comments pull-left"></i><?php // endif;                                                                                                                ?>
-                                <?php // if (isset($myfile->userSpacialProject[0]) && $myfile->userSpacialProject[0]->status == 1):      ?>
-                                                                        <span class="label label-danger">R</span>
-                                <?php // elseif (isset($myfile->userSpacialProject[0]) && $myfile->userSpacialProject[0]->status == 2):      ?>
-                                                                        <span class="label label-danger">S</span>
-                                <?php // endif;      ?>
-                                                                </h3>
-                                                                <p>วันที่สร้าง :<?php // echo $this->dateThai($myfile->createDateTime, 3, TRUE);                                                                                                                  ?></p>
-                                                                <p>วันที่แก้ไข :<?php // echo $this->dateThai($myfile->updateDateTime, 2, TRUE)                                                                                                                  ?></p>
-                                                                <p>จังหวัดที่ส่ง : <?php // echo Province::model()->findByPk($myfile->provinceId)->provinceName;                                                                                                                  ?></p>
-                                                            </a>
-                                                        </div>-->
-                                <!--</div>-->
-                                <?php // $i++;   ?>
-                                <?php // endforeach;  ?>
                             </div>
                             <?php
                         endif;
                     }
                     ?>
-                    <!--<div id="2">-->
-                    <?php // $i = 0;     ?>
-                    <?php // foreach($myfileHistoryArray as $myfile):   ?>
-                    <!--<div class='col-lg-3 col-md-3 col-sm-12'>-->
-                    <!--<div class="blog-item">-->
-                            <!--<a class="btn <?php // echo ($myfile->status == 1) ? "btn-success" : "btn-primary"                                                                                                                  ?> col-md-12"  href="<?php // echo Yii::app()->createUrl('/index.php/myfile/madrid/view/id/' . $myfile->orderId);                                                                                                                  ?>">-->
-                                    <!--<h3><?php // echo $myfile->title;                                                                                                                  ?><?php // if($myfile->status == 1):                                                                                                                  ?><i class="fa fa-comments pull-left"></i><?php // endif;                                                                                                                  ?></h3>-->
-                                    <!--<p>วันที่สร้าง :<?php // echo $this->dateThai($myfile->createDateTime, 3, TRUE);                                                                                                                  ?></p>-->
-                                    <!--<p>วันที่แก้ไข :<?php // echo $this->dateThai($myfile->updateDateTime, 2, TRUE)                                                                                                                  ?></p>-->
-                                    <!--<p>จังหวัดที่ส่ง : <?php // echo Province::model()->findByPk($myfile->provinceId)->provinceName;                                                                                                                  ?></p>-->
-                    <!--</a>-->
-                    <!--</div>-->
-                    <!--</div>-->
-                    <?php // $i++;     ?>
-                    <?php // endforeach;   ?>
-                    <!--</div>-->
+
                 </div>
             </div>
             <?php
@@ -95,70 +67,8 @@ $this->breadcrumbs = array(
         ?>
     </div>
 </div>
-
-
-<!--<div id="product-single">
-
-     Product 
-    <div class="product-single">
-
-        <div class="row">
-
-             Product Images Carousel 
-            <div class="col-lg-5 col-md-5 col-sm-5 product-single-image">-->
-<?php
-//				$this->renderPartial('//layouts/_product_slider', array(
-//					'images'=>$images));
+*/
 ?>
-<!--</div>-->
-<!-- /Product Images Carousel -->
-
-
-
-
-<!--</div>-->
-<!--
-    </div>
-     /Product 
-
-     Product tabs 
-
-
-</div>
-
-<div class="page-content" id="showProduct">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12">
-            <table class="table table-bordered atechwindow-items">
-                <tr>
-                    <th>รุ่น</th>
-                    <th>รหัสสินค้า</th>
-                    <th>รายละเอียด</th>
-                    <th>ขนาด(มิลลิเมตร)</th>
-                    <th>สี</th>
-                    <th>ราคา</th>
-                    <th>Qty</th>
-                    <th>Actions</th>
-                </tr>
-                <tbody id="productItems"></tbody>
-            </table>
-        </div>
-
-        <div class="col-lg-3 col-md-3 col-sm-3 col-md-offset-9">
-
-                                    <div class="product-actions">
-                                                        <span class="add-to-cart">
-                                                                <span class="action-wrapper">
-                                                                        <i class="fa fa-shopping-cart"></i>
-                                                                        <span class="action-name">View cart</span>
-                                                                </span>
-                                                        </span>
-                                                </div>
-        </div>
-
-    </div>
-</div>-->
-
 <?php
 Yii::app()->clientScript->registerScript('catIndex', "
 ", CClientScript::POS_READY);
