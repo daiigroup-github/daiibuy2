77c9f89ab13e7152050f8abfd839a14fd5181e24
xaqu <pth@Pirapats-MacBook-Pro.local>
2015-11-17 09:15:12 +0700

    sync
    sync

--- protected/modules/checkout/controllers/CartController.php
+++ protected/modules/checkout/controllers/CartController.php
@@ -10,55 +10,55 @@ class CartController extends MasterCheckoutController
 
 		$orders = array();
 
-		if(isset(Yii::app()->user->id))
+		if (isset(Yii::app()->user->id))
 		{
 			$orders = Order::model()->findAll(array(
-				'condition'=>'type&' . Order::ORDER_TYPE_CART . ' > 0 AND userId=:userId AND supplierId=:supplierId',
-				'params'=>array(
-					':userId'=>Yii::app()->user->id,
-					':supplierId'=>$id,
+				'condition' => 'type&' . Order::ORDER_TYPE_CART . ' > 0 AND userId=:userId AND supplierId=:supplierId',
+				'params' => array(
+					':userId' => Yii::app()->user->id,
+					':supplierId' => $id,
 				),
-				'order'=>'type, orderId'
+				'order' => 'type, orderId'
 			));
 		}
 		else
 		{
 			$orders = Order::model()->findAll(array(
-				'condition'=>'type&' . Order::ORDER_TYPE_CART . ' > 0 AND token=:token AND supplierId=:supplierId',
-				'params'=>array(
-					':token'=>$daiibuy->token,
-					':supplierId'=>$id,
+				'condition' => 'type&' . Order::ORDER_TYPE_CART . ' > 0 AND token=:token AND supplierId=:supplierId',
+				'params' => array(
+					':token' => $daiibuy->token,
+					':supplierId' => $id,
 				),
 			));
 		}
 
 		$desc = array();
-		if($id == 4)
+		if ($id == 4)
 		{
-			if(isset($orders[0]))
+			if (isset($orders[0]))
 			{
 				$i = 0;
 				$order = $orders[0];
-                //                throw new Exception(print_r($order->orderItems, true));
-                foreach($order->orderItems as $orderItem)
+				//                throw new Exception(print_r($order->orderItems, true));
+				foreach ($order->orderItems as $orderItem)
 				{
-//                    throw new Exception(print_r($orderItem->product->category2ToProducts, true));
+//					throw new Exception(print_r($orderItem->product->category2ToProducts, true));
 					$category = (isset($orderItem->product->category2ToProducts[1]) && (isset($orderItem->product->category2ToProducts[1]->type))) ? $orderItem->product->category2ToProducts[1]->category : $orderItem->product->category2ToProducts[0]->category;
 					$category2 = isset($orderItem->product->category2ToProducts[1]) ? $orderItem->product->category2ToProducts[1]->category2 : $orderItem->product->category2ToProducts[0]->category2;
 					$categoryToSub = CategoryToSub::model()->find(array(
-						'condition'=>'categoryId=:categoryId AND subCategoryId=:subCategoryId',
-						'params'=>array(
-							':categoryId'=>$category->categoryId,
-							':subCategoryId'=>$category2->categoryId
+						'condition' => 'categoryId=:categoryId AND subCategoryId=:subCategoryId',
+						'params' => array(
+							':categoryId' => $category->categoryId,
+							':subCategoryId' => $category2->categoryId
 						)
 					));
 //					throw new Exception(print_r($category->categoryId . ", " . $category2->categoryId, true));
 
 					$categoryStakeProvinceModel = CategoryStakeProvince::model()->find(array(
-						'condition'=>'categoryId=:categoryId AND provinceId=:provinceId',
-						'params'=>array(
-							':categoryId'=>$category2->categoryId,
-							':provinceId'=>$daiibuy->provinceId
+						'condition' => 'categoryId=:categoryId AND provinceId=:provinceId',
+						'params' => array(
+							':categoryId' => $category2->categoryId,
+							':provinceId' => $daiibuy->provinceId
 						)
 					));
 //					throw new Exception(print_r($orderItem->product->category2ToProducts, true));
@@ -67,20 +67,20 @@ class CartController extends MasterCheckoutController
 					$desc[$i]["title"] = $category->title . ' : ' . $category2->title;
 					$desc[$i]['detail'] = str_replace('{{pile}}', isset($categoryStakeProvinceModel->stake) ? $categoryStakeProvinceModel->stake : "", isset($categoryToSub->payCondition) ? $categoryToSub->payCondition : "");
 //					throw new Exception(print_r($categoryToSub->payCondition, true));
-					$j=1;
-					$category2ToProduct = isset($orderItem->product->category2ToProducts[1]) ? $orderItem->product->category2ToProducts[1] :$orderItem->product->category2ToProducts[0];
+					$j = 1;
+					$category2ToProduct = isset($orderItem->product->category2ToProducts[1]) ? $orderItem->product->category2ToProducts[1] : $orderItem->product->category2ToProducts[0];
 					$brandId = $category2ToProduct->brandId;
 					$brandModelId = $category2ToProduct->brandModelId;
 					$category1Id = $category2ToProduct->category1Id;
 					$category2Id = $category2ToProduct->category2Id;
-				
+
 					$category2ToProducts = Category2ToProduct::model()->findAll(array(
-						'condition'=>'brandId=:brandId and brandModelId=:brandModelId and category1Id=:category1Id and category2Id=:category2Id',
-						'params'=>array(
-							':brandId'=>$brandId,
-							':brandModelId'=>$brandModelId,
-							':category1Id'=>$category1Id,
-							':category2Id'=>$category2Id,
+						'condition' => 'brandId=:brandId and brandModelId=:brandModelId and category1Id=:category1Id and category2Id=:category2Id',
+						'params' => array(
+							':brandId' => $brandId,
+							':brandModelId' => $brandModelId,
+							':category1Id' => $category1Id,
+							':category2Id' => $category2Id,
 						)
 					));
 
@@ -89,17 +89,16 @@ class CartController extends MasterCheckoutController
 					{
 //						throw new Exception(print_r($desc[$i]['detail'], true));
 						$desc[$i]['detail'] = str_replace("{{" . "price{$j}" . "}}", $c2tp->product->calProductPriceGinza($order->orderId), $desc[$i]['detail']);
-                        $j++;
+						$j++;
 					}
 
 
-                    $i++;
-                                        
-                }
+					$i++;
+				}
 			}
 		}
-        //        throw new Exception(print_r($desc, true));
-		
+		//        throw new Exception(print_r($desc, true));
+
 		if ($id == 5)
 		{
 			$orderSummary = Order::model()->sumOrderTotalByProductIdAndQuantity(NULL, 0, $id, 100000, TRUE, NULL);
@@ -111,11 +110,11 @@ class CartController extends MasterCheckoutController
 		}
 		$supplierModel = Supplier::model()->findByPk($id);
 		$this->render('cart', array(
-			'orders'=>$orders,
-			'orderSummary'=>$orderSummary,
-			'supplierId'=>$id,
-            'supplierModel'=>$supplierModel,
-			'desc'=>$desc
+			'orders' => $orders,
+			'orderSummary' => $orderSummary,
+			'supplierId' => $id,
+			'supplierModel' => $supplierModel,
+			'desc' => $desc
 		));
 	}
 
@@ -127,15 +126,15 @@ class CartController extends MasterCheckoutController
 
 	public function actionUpdateCart()
 	{
-		if(isset($_POST['quantity']))
+		if (isset($_POST['quantity']))
 		{
 			$res = [];
 
-			foreach($_POST['quantity'] as $orderItemsId=> $quantity)
+			foreach ($_POST['quantity'] as $orderItemsId => $quantity)
 			{
 				$orderItem = OrderItems::model()->findByPk($orderItemsId);
 
-				if($orderItem->quantity == $quantity)
+				if ($orderItem->quantity == $quantity)
 				{
 					continue;
 				}
@@ -164,7 +163,7 @@ class CartController extends MasterCheckoutController
 	{
 		$model = Order::model()->findByPk($id);
 		$supplierId = $model->supplierId;
-		if($model->type & 1 > 0)
+		if ($model->type & 1 > 0)
 		{
 			$model->type = 1;
 			$model->save(FALSE);
@@ -176,7 +175,7 @@ class CartController extends MasterCheckoutController
 		}
 		$this->redirect(array(
 			'index',
-			"id"=>$supplierId));
+			"id" => $supplierId));
 	}
 
 }
--- protected/modules/checkout/views/cart/_desc_tab.php
+++ protected/modules/checkout/views/cart/_desc_tab.php
@@ -4,38 +4,38 @@
             <div class="tab-heading">
 				<?php
 //				throw new Exception(print_r($tabs, true));
-$i = 1;
-                foreach($tabs as $tab)
-                {
-				//													throw new Exception(print_r($tab, true));
-	$title = $tab["title"];
-	$tabId = isset($tab['id']) ? $tab['id'] : 'tab' . $i;
+				$i = 1;
+				foreach ($tabs as $tab)
+				{
+					//													throw new Exception(print_r($tab, true));
+					$title = $tab["title"];
+					$tabId = isset($tab['id']) ? $tab['id'] : 'tab' . $i;
 
-				echo CHtml::link($title . ' ', '#' . $tabId, array(
-		'class' => 'button big',
-				'style' => 'font-size:12px'));
-				$i++;
-}
-                ?>
+					echo CHtml::link($title . ' ', '#' . $tabId, array(
+						'class' => 'button big',
+						'style' => 'font-size:12px'));
+					$i++;
+				}
+				?>
             </div>
 
             <div class="page-content tab-content">
-                <?php
-                $i = 1;
-                foreach($tabs as $tab)
-                {
-                    $title = $tab['title'];
-                    //$tabHeading .= "<a href=\"#tab$i\" class=\"button big\">$title</a> ";
-                    $tabId = isset($tab['id']) ? $tab['id'] : 'tab' . $i;
-                    ?>
-                    <div id="<?php echo $tabId ?>">
-                        <?php
-                        echo $tab['detail'];
-                        ?>
-                    </div>
-                <?php
-                }
-                ?>
+				<?php
+				$i = 1;
+				foreach ($tabs as $tab)
+				{
+					$title = $tab['title'];
+					//$tabHeading .= "<a href=\"#tab$i\" class=\"button big\">$title</a> ";
+					$tabId = isset($tab['id']) ? $tab['id'] : 'tab' . $i;
+					?>
+					<div id="<?php echo $tabId ?>">
+						<?php
+						echo $tab['detail'];
+						?>
+					</div>
+					<?php
+				}
+				?>
             </div>
         </div>
     </div>
