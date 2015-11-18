c1d92663901515958d13df6453beb51e5087c5dc
xaqu <pth@Pirapats-MacBook-Pro.local>
2015-11-18 14:20:17 +0700

    fixed furniture buy by credit card bug
    fixed

--- protected/modules/checkout/views/step/e_payment/_parameter_form.php
+++ protected/modules/checkout/views/step/e_payment/_parameter_form.php
@@ -98,16 +98,17 @@ echo CHtml::hiddenField("merchant_defined_data10", $ePayment->ePaymentTel);
 //Merchant Parameter
 //
 $i = 0;
-foreach($model->orders as $order)
+foreach ($model->orders as $order)
 {
-	foreach($order->orderItems as $item)
+//	throw new Exception(print_r($order->orderItems, true));
+	foreach ($order->orderItems as $item)
 	{
 		$unsignedField .=",item_" . $i . "_unit_price,item_" . $i . "_tax_amount,item_" . $i . "_code,item_" . $i . "_name,item_" . $i . "_sku,item_" . $i . "_quantity";
 		echo CHtml::hiddenField("item_" . $i . "_unit_price", number_format($item->price, 2, ".", ""));
 		echo CHtml::hiddenField("item_" . $i . "_tax_amount", number_format(($item->total / 1.07), 2, ".", ""));
 		echo CHtml::hiddenField("item_" . $i . "_code", "default");
-		echo CHtml::hiddenField("item_" . $i . "_name", $item->product->name);
-		echo CHtml::hiddenField("item_" . $i . "_sku", $item->product->code);
+		echo CHtml::hiddenField("item_" . $i . "_name", isset($item->product->name) ? $item->product->name : $item->title);
+		echo CHtml::hiddenField("item_" . $i . "_sku", isset($item->product->name) ? $item->product->name : "-");
 		echo CHtml::hiddenField("item_" . $i . "_quantity", $item->quantity);
 		$i++;
 	}
