ce3896a102305073a4ee93c8825065d85c19960b
kamyjapkung <kamyjap@gmail.com>
2016-01-14 08:45:15 +0700

    test e-paymentUrl
    xxx

--- protected/modules/checkout/views/step/e_payment/payment_confirmation.php
+++ protected/modules/checkout/views/step/e_payment/payment_confirmation.php
@@ -11,66 +11,64 @@ Yii::app()->clientScript->registerScript('load', "$(document).ready(function () 
 	});");
 ?>
 <!--<script >
-	$(document).ready(function () {
-		setTimeout(function () {
-			$('#confirmationForm').submit();
-			alert(111);
-		}, 1000
-				);
-	});
+    $(document).ready(function () {
+        setTimeout(function () {
+            $('#confirmationForm').submit();
+            alert(111);
+        }, 1000
+                );
+    });
 </script>-->
 
 
 
 <?php
-foreach($_REQUEST as $name=> $value)
-{
-	if(strtolower($name) == 'submit')
-		continue;
+foreach ($_REQUEST as $name => $value) {
+    if (strtolower($name) == 'submit')
+        continue;
 
-	$params[$name] = $value;
+    $params[$name] = $value;
 }
 ?>
 
 <div class="row sidebar-box blue">
 
-	<div class="col-lg-12 col-md-12 col-sm-12">
+    <div class="col-lg-12 col-md-12 col-sm-12">
 
-		<div class="sidebar-box-heading">
-			<i class="icons icon-box-2"></i>
-			<h4>Waiting e-Payment</h4>
-		</div>
-		<div class="sidebar-box-content sidebar-padding-box">
-			<!--<div class="row">-->
-			<!--<div class="col-md-12" style="height: 100px">-->
-			<?php
+        <div class="sidebar-box-heading">
+            <i class="icons icon-box-2"></i>
+            <h4>Waiting e-Payment</h4>
+        </div>
+        <div class="sidebar-box-content sidebar-padding-box">
+            <!--<div class="row">-->
+            <!--<div class="col-md-12" style="height: 100px">-->
+            <?php
 //					echo CHtml::image(Yii::app()->baseUrl . "/images/logo.png", "", array(
 //						"style"=>"width:250px"));
-			?>
-			<!--</div>-->
-			<!--</div>-->
-			<div class="row">
-				<div class="col-md-12">
-					<i class="icon-spinner icon-spin icon-3x"></i>&nbsp;&nbsp;&nbsp;&nbsp;<span  style="font-size: 40px;font-weight: bold">ระบบกำลังดำเนินการ ติดต่อธนาคารเพื่อชำระเงิน</span>
-				</div>
-			</div>
-		</div>
-	</div>
+            ?>
+            <!--</div>-->
+            <!--</div>-->
+            <div class="row">
+                <div class="col-md-12">
+                    <i class="icon-spinner icon-spin icon-3x"></i>&nbsp;&nbsp;&nbsp;&nbsp;<span  style="font-size: 40px;font-weight: bold">ระบบกำลังดำเนินการ ติดต่อธนาคารเพื่อชำระเงิน</span>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
 
-
+<?php echo $ePayment->ePaymentUrl; ?>
 <form id="confirmationForm" action="<?php echo $ePayment->ePaymentUrl; ?>" method="post">
-	<?php
-	foreach($params as $name=> $value)
-	{
-		echo "<input type=\"hidden\" id=\"" . $name . "\" name=\"" . $name . "\" value=\"" . $value . "\"/>\n";
-	}
+    <?php
+    foreach ($params as $name => $value) {
+        echo "<input type=\"hidden\" id=\"" . $name . "\" name=\"" . $name . "\" value=\"" . $value . "\"/>\n";
+    }
 
-	echo "<input type=\"hidden\" id=\"signature\" name=\"signature\" value=\"" . sign($params) . "\"/>\n";
-	?>
+    echo "<input type=\"hidden\" id=\"signature\" name=\"signature\" value=\"" . sign($params) . "\"/>\n";
+    ?>
 
-	<!--<input type="submit" id="submit" value="submit" />-->
+    <!--<input type="submit" id="submit" value="submit" />-->
 
 </form>
 
